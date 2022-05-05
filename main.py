f41b6243aa9ded39e0fb8aa386a4bc11a0827600
LuosRemmus <17summersoul17@gmail.com>
2022-05-05 10:45:05 +0300

    rework

--- main.py
+++ main.py
@@ -1,71 +1,51 @@
 from datetime import datetime
 import requests
 import telebot
+from telebot import types
 from bs4 import BeautifulSoup
 
 Token = '5377279394:AAEo7i23FpDgPeI9FizGOhMUo24e0m_MMBU'
+
+date = datetime.now().strftime('%d/%m/%Y')
 
 response = requests.get("https://finance.rambler.ru/currencies/")
 soup = BeautifulSoup(response.text, 'lxml')
 
 block = soup.find('div', class_='finance-currency-table__table')
-head = block.find('div', class_='finance-currency-table__head')
-body = block.find('div', class_='finance-currency-table__body')
-currency = body.find_all('a', class_='finance-currency-table__tr')
 
-header = "    ".join(head.text.split())
+header = block.find('div', class_='finance-currency-table__head').text.split()
+header[3] += f' {header[4]}'
+del header[4]
+
+body = block.find('div', class_='finance-currency-table__body')
+
+c_code = body.find_all('div', class_='finance-currency-table__cell--code')
+c_denomination = body.find_all('div', class_='finance-currency-table__cell--denomination')
+c_currency = body.find_all('div', class_='finance-currency-table__cell--currency')
+c_value = body.find_all('div', class_='finance-currency-table__cell--value')
+c_change = body.find_all('div', class_='finance-currency-table__cell--change')
+c_percent = body.find_all('div', class_='finance-currency-table__cell--percent')
 
 currency_dct = {}
-emoji_dct = {}
+
+for i in range(len(c_code)):
+    currency_dct[c_code[i].text.rstrip().lstrip()] = (c_denomination[i].text.rstrip().lstrip(),
+                                                      c_currency[i].text.rstrip().lstrip(),
+                                                      c_value[i].text.rstrip().lstrip(),
+                                                      c_change[i].text.rstrip().lstrip(),
+                                                      c_percent[i].text.rstrip().lstrip())
+
 emoji_list = ['🇦🇲', '🇦🇺', '🇦🇿', '🇧🇬', '🇧🇷', '🇧🇾', '🇨🇦', '🇨🇭', '🇨🇳', '🇨🇿', '🇳🇱', '🇪🇺', '🇬🇧',
               '🇭🇺', '🇮🇳', '🇯🇵', '🇰🇿',
               '🇰🇷', '🇰🇿', '🇲🇩', '🇳🇴', '🇵🇱', '🇷🇴', '🇸🇪', '🇸🇬', '🇹🇯', '🇹🇲', '🇹🇷', '🇺🇦', '🇺🇸',
               '🇺🇿', '💵', '🇿🇦']
-for c in currency:
-    currency_dct[c.text.split()[0]] = tuple(c.text.split()[1:])
-    emoji_dct[c.text.split()[0]] = ''
 
-cnt_v = 0
-for value in emoji_dct:
-    try:
-        emoji_dct[value] = emoji_list[cnt_v]
-    except IndexError as e:
-        print(e)
-    cnt_v += 1
+Abbreviations = tuple(["AMD", "AUD", "AZN", "BGN", "BRL", "BYN", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR",
+                       "GBP", "HUF", "INR", "JPY", "KGS", "KRW", "KZT", "MDL", "NOK", "PLN", "RON", "SEK",
+                       "SGD", "TJS", "TMT", "TRY", "UAH", "USD", "UZS", "XDR", "ZAR"])
 
 
-def get_data():
-    body_ = "\n".join(["    ".join(c.text.split()) for c in currency])
-    return f"{datetime.now().strftime('%d/%m/%Y, %H:%M')}\n" \
-           f"{header}\n" \
-           f"{body_}"
-
-
-def sorted_by_value():
-    body_ = sorted([i.text.split() for i in currency], key=lambda x: float(x[-3]) / float(x[1]), reverse=True)
-    s = "\n".join(["    ".join(b) for b in body_])
-
-    return f"{datetime.now().strftime('%d/%m/%Y, %H:%M')}\n" \
-           f"{header}\n" \
-           f"{s}"
-
-
-def sorted_by_ud_percent():
-    body_ = sorted([i.text.split() for i in currency], key=lambda x: float(x[-1][1:-1]), reverse=True)
-    s = "\n".join(["   ".join(b) for b in body_])
-
-    return f"{datetime.now().strftime('%d/%m/%Y, %H:%M')}\n" \
-           f"{header}\n" \
-           f"{s}"
-
-
-def sorted_by_ud_value_():
-    body_ = sorted([i.text.split() for i in currency], key=lambda x: float(x[-2][1:]), reverse=True)
-    s = "\n".join(["    ".join(b) for b in body_])
-
-    return f"{datetime.now().strftime('%d/%m/%Y, %H:%M')}\n" \
-           f"{header}\n" \
-           f"{s}"
+# TELEBOT COMMANDS
 
 
 def telegram_bot(token):
@@ -73,48 +53,38 @@ def telegram_bot(token):
 
     @bot.message_handler(commands=["start"])
     def start_message(message):
-        bot.send_message(message.chat.id, "Привет!\n"
-                                          "Я бот, который покажет тебе курс валют относительно рубля.\n"
-                                          "Чтобы посмотреть все команды - нажми /info")
+        bot.send_photo(message.chat.id,
+                       'https://avatars.mds.yandex.net/get-zen_doc/2814495'
+                       '/pub_5f85eed13940476c66f965d8_5f85ef84ae6a9712bf416ade/scale_1200',
+                       f"Привет <b>{message.from_user.first_name}</b>!\n"
+                       f"Я бот, который покажет тебе курс валют как относительно рубля.\n"
+                       f"Чтобы посмотреть курс валют - нажми /currency", parse_mode='html')
 
-    @bot.message_handler(commands=["info"])
-    def info(message):
-        bot.send_message(message.chat.id, "/info - Посмотреть все команды\n\n"
-                                          "/sort_by_alphabet - Посмотреть курс валют относительно рубля"
-                                          "(Сортировка по алфавиту)\n"
-                                          "/sorted_by_value - сортировка от большего к меньшему (по значению)\n"
-                                          "/sorted_by_ud_percent - сортировка по проценту изменения курса\n"
-                                          "/sorted_by_ud_value - сортировка по значению изменения курса\n\n"
-                                          "Чтобы посмотреть курс конеретной валюты "
-                                          "введите её аббревиатуру в верхнем регистре через slash (например /USD)")
 
-    @bot.message_handler(commands=["sort_by_alphabet"])
-    def send_text(message):
-        try:
-            bot.send_message(message.chat.id, get_data())
-        except Exception as e:
-            print(e)
-            bot.send_message(message.chat.id, "Something goes wrong... Try later")
-
-    @bot.message_handler(commands=["sorted_by_value"])
-    def sort_currency_by_value(message):
-        bot.send_message(message.chat.id, sorted_by_value())
-
-    @bot.message_handler(commands=["sorted_by_ud_percent"])
-    def sort_currency_by_ud_percent(message):
-        bot.send_message(message.chat.id, sorted_by_ud_percent())
-
-    @bot.message_handler(commands=["sorted_by_ud_value"])
-    def sort_currency_by_ud_value(message):
-        bot.send_message(message.chat.id, sorted_by_ud_value_())
-
-    @bot.message_handler(commands=["AMD", "AUD", "AZN", "BGN", "BRL", "BYN", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR",
-                                   "GBP", "HUF", "INR", "JPY", "KGS", "KRW", "KZT", "MLD", "NOK", "PLN", "RON", "SEK",
-                                   "SGD", "TJS", "TMT", "TRY", "UAH", "USD", "UZS", "XDR", "ZAR"])
+    @bot.message_handler(commands=['currency'])
     def get_currency(message):
-        crn = '    '.join(currency_dct[message.text[1:]])
-        msg = f"{datetime.now().strftime('%d/%m/%Y, %H:%M')}\n{header}\n{emoji_dct[message.text[1:]]}{crn}"
-        bot.send_message(message.chat.id, msg)
+        markup = types.ReplyKeyboardMarkup(row_width=6, resize_keyboard=True)
+        btns = []
+        for i in range(len(Abbreviations)):
+            try:
+                btns.append(types.KeyboardButton(f'{emoji_list[i]}{Abbreviations[i]}'))
+            except KeyError as k:
+                print(k)
+        markup.add(*tuple(btns))
+        bot.send_message(message.chat.id, "Выбери одну из валют", reply_markup=markup)
+
+    @bot.message_handler(content_types=['text'])
+    def Specific(message):
+        if message.text[0] == '💵':
+            cur = currency_dct[message.text[1:]]
+            mes = f"{date}\n{'     '.join(header[1:])}\n{'     '.join(cur)}"
+            bot.send_message(message.chat.id, mes)
+        elif message.text[:2] in emoji_list:
+            cur = currency_dct[message.text[2:]]
+            mes = f"{date}\n{'     '.join(header[1:])}\n{'     '.join(cur)}"
+            bot.send_message(message.chat.id, mes)
+        else:
+            bot.send_message(message.chat.id, "I don't understand u...")
 
     bot.polling()
 
